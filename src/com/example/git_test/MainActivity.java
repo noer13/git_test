01b9ed531822ed32ff93af9f4ab44d085651dbcd
Yuriy Protskiv <iquadre@gmail.com>
2013-11-29 12:01:44 +0200

    LOG

--- src/com/example/git_test/MainActivity.java
+++ src/com/example/git_test/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.git_test;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	
+	final String LOG = "myLogs";
+	final String LOG_TEXT = "Yura";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,6 +20,7 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		Log.d(LOG, LOG_TEXT);
 		return true;
 	}
 
